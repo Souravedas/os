21c15e68dc5b1b02ff8966232fea2e0a13240cc6
Souravedas <144267597+Souravedas@users.noreply.github.com>
2023-09-09 12:51:14 +0600

    Add files via upload

---
+++ fcfs3.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+// Structure to represent a process
+struct Process {
+    int id;
+    int arrival_time;
+    int burst_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+};
+
+// Function to perform FCFS scheduling
+void fcfs(struct Process processes[], int n) {
+    int current_time = 0; // Current time
+    float total_turnaround_time = 0;
+    float total_waiting_time = 0;
+
+    printf("Process\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+
+    for (int i = 0; i < n; i++) {
+        // Wait for the process to arrive if necessary
+        if (current_time < processes[i].arrival_time) {
+            current_time = processes[i].arrival_time;
+        }
+
+        // Update completion time
+        processes[i].completion_time = current_time + processes[i].burst_time;
+
+        // Update turnaround time and waiting time
+        processes[i].turnaround_time = processes[i].completion_time - processes[i].arrival_time;
+        processes[i].waiting_time = processes[i].turnaround_time - processes[i].burst_time;
+
+        // Update total turnaround and waiting times
+        total_turnaround_time += processes[i].turnaround_time;
+        total_waiting_time += processes[i].waiting_time;
+
+        // Print process details
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
+               processes[i].id, processes[i].arrival_time, processes[i].burst_time,
+               processes[i].completion_time, processes[i].turnaround_time, processes[i].waiting_time);
+
+        // Update current time
+        current_time = processes[i].completion_time;
+    }
+
+    // Calculate and print average turnaround and waiting times
+    float avg_turnaround_time = total_turnaround_time / n;
+    float avg_waiting_time = total_waiting_time / n;
+    printf("\nAverage Turnaround Time: %.2f", avg_turnaround_time);
+    printf("\nAverage Waiting Time: %.2f\n", avg_waiting_time);
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    struct Process processes[n];
+
+    printf("Enter arrival time and burst time for each process:\n");
+    for (int i = 0; i < n; i++) {
+        processes[i].id = i + 1;
+        printf("Process P%d:\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &processes[i].arrival_time);
+        printf("Burst Time: ");
+        scanf("%d", &processes[i].burst_time);
+    }
+
+    // Sort processes by arrival time (FCFS)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (processes[j].arrival_time > processes[j + 1].arrival_time) {
+                // Swap processes
+                struct Process temp = processes[j];
+                processes[j] = processes[j + 1];
+                processes[j + 1] = temp;
+            }
+        }
+    }
+
+    fcfs(processes, n);
+
+    return 0;
+}

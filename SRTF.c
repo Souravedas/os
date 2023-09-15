7aa1a9aca0ffee38bf41373bb59256a47064cb9d
Souravedas <144267597+Souravedas@users.noreply.github.com>
2023-09-15 23:37:51 +0600

    Update SRTF.c

--- SRTF.c
+++ SRTF.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
 
-// Function to perform Preemptive Shortest Remaining Time First scheduling
 void srtf(int n);
 
 int main() {
@@ -15,28 +14,27 @@ int main() {
     return 0;
 }
 
-// Function to perform Preemptive Shortest Remaining Time First scheduling
-void srtf(int n) {
-    int at[n]; // Array to store arrival time for each process
-    int bt[n]; // Array to store burst time for each process
-    int rt[n]; // Array to store remaining time for each process
-    int ct[n]; // Array to store completion time for each process
-    int tat[n]; // Array to store turnaround time for each process
-    int wt[n]; // Array to store waiting time for each process
 
-    // Input arrival time and burst time for each process
+void srtf(int n) {
+    int at[n]; 
+    int bt[n]; 
+    int rt[n]; 
+    int ct[n]; 
+    int tat[n];
+    int wt[n]; 
+
     for (int i = 0; i < n; i++) {
         printf("Process P%d:\n", i + 1);
         printf("Arrival Time: ");
         scanf("%d", &at[i]);
         printf("Burst Time: ");
         scanf("%d", &bt[i]);
-        rt[i] = bt[i]; // Initialize remaining time to burst time
+        rt[i] = bt[i]; 
     }
 
-    int current_time = 0; // Current time
-    int completed = 0; // Counter for completed processes
-    int min_rt_index; // Index of the process with the minimum remaining time
+    int current_time = 0; 
+    int completed = 0; 
+    int min_rt_index; 
 
     printf("Process\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
 
@@ -51,13 +49,6 @@ void srtf(int n) {
             }
         }
 
-        // No process found to execute
-        if (min_rt == INT_MAX) {
-            current_time++;
-            continue;
-        }
-
-        // Execute the process with the minimum remaining time for one time unit
         rt[min_rt_index]--;
 
         if (rt[min_rt_index] == 0) {
@@ -66,14 +57,12 @@ void srtf(int n) {
             tat[min_rt_index] = ct[min_rt_index] - at[min_rt_index];
             wt[min_rt_index] = tat[min_rt_index] - bt[min_rt_index];
 
-            // Update total turnaround and waiting times
             current_time = ct[min_rt_index];
         } else {
             current_time++;
         }
     }
 
-    // Print process details
     float total_tat = 0;
     float total_wt = 0;
     for (int i = 0; i < n; i++) {
@@ -83,7 +72,6 @@ void srtf(int n) {
         total_wt += wt[i];
     }
 
-    // Calculate and print average turnaround and waiting times
     float avg_tat = total_tat / n;
     float avg_wt = total_wt / n;
     printf("\nAverage Turnaround Time: %.2f", avg_tat);
